66e24d5c83c4bfaac6d40dd8ce2628283ec5ceb3
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-18 14:20:30 +0200

    Implementing scanner

--- Sandbox.java
+++ Sandbox.java
@@ -1,17 +1,29 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Sandbox {
     public static void main(String[] args) {
-        String[] cars = new String[3];
 
-        cars[0] = "Camero";
-        cars[1] = "Corvette";
-        cars[2] = "Tesla";
+        Scanner scanner = new Scanner(System.in);
+
+        String[] foods;
+        int size;
+
+        System.out.print("what # of food do you want?");
+        size = scanner.nextInt();
+        scanner.nextLine();
+
+        foods = new String[size];
 
 
-        for (String car:cars) {
-            System.out.println(car);
+        for(int i = 0; i < foods.length; i++) {
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
         }
+
+        for(String food: foods) {
+            System.out.println(food);
+        }
+        scanner.close();
     }
 }
 
