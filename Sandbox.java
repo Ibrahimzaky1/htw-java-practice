e5f543c11dbc18481d9391ee2ef27535e94f0204
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-18 12:23:31 +0200

    Arrays in Java

--- Sandbox.java
+++ Sandbox.java
@@ -1,22 +1,20 @@
+import java.util.Arrays;
+
 public class Sandbox {
-
-
-    
-    public static int factorial(int input) {
-        if (input <= 1) return 1;
-        return input * factorial(input - 1);
-    }
-    
-    public static int factorialSum(int input) {
-        if (input <= 1) return 1; 
-        return factorial(input) + factorialSum(input - 1);
-    }
-
     public static void main(String[] args) {
-        int result = factorialSum(5);
-        System.out.println(result);
+        String[] fruits = {"Apple", "Orange", "Banana", "Coconut"};
+
+        //fruit[0] = "Pineapple";
+
+        //int numOfFruit =fruit.length;
+
+        //Arrays.sort(fruits);
+        Arrays.fill(fruits, "Pineapple");
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
-    
+    }
 }
 
 
