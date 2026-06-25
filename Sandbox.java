6d830027c7836340e4b4d7361aacbd7e791c626c
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-25 11:41:12 +0200

    More throws

--- Sandbox.java
+++ Sandbox.java
@@ -1,15 +1,15 @@
+import java.io.IOException;
+
 public class Sandbox {
-     public static void main(String[] args) {
-       try {
-          Write();
-       }
-       catch (IOException e) {
-          System.out.println("Something went wrong while writing the file.");
-       }
+     public static void main(String[] args) throws IOException {
+          arrayPractice(3);
      }
-     public static void Write() throws IOException {
-          BufferedWriter W = new BufferedWriter(new FileWriter("name.txt"));
-          W.write("test");
-          W.close();
+
+     public static void arrayPractice(int i) {
+          int a[] = new int[5];
+
+          if (i < 0 ||i >= 5) {
+               throw new ArrayIndexOutOfBoundsException("Hey, don't put indexes too high");
+          }
      }
 }
