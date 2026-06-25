968780d45203a56567042cc1bfe4b8076b3a8e1e
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-25 11:01:12 +0200

    IOException

--- Sandbox.java
+++ Sandbox.java
@@ -1,34 +1,15 @@
 public class Sandbox {
-     public static double mittelwert(int[] arr) throws NullPointerException {
-        if (arr == null) {
-            throw new NullPointerException("Array ist null!");
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-
-        return (double) sum / arr.length;
-    }
-
-    // Compares the averages of two arrays
-    public static boolean vergleich(int[] a, int[] b) {
-        if (mittelwert(a) == mittelwert(b)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-
-        int[] x = {1, 2, 3};
-        int[] y = null;
-
-        boolean result = vergleich(x, y);
-
-        System.out.println(result);
-    }
+     public static void main(String[] args) {
+       try {
+          Write();
+       }
+       catch (IOException e) {
+          System.out.println("Something went wrong while writing the file.");
+       }
+     }
+     public static void Write() throws IOException {
+          BufferedWriter W = new BufferedWriter(new FileWriter("name.txt"));
+          W.write("test");
+          W.close();
+     }
 }
