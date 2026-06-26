76bf3b7719651f0b61e969240a81ecaa9098c634
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-26 08:01:50 +0200

    New practice code for try catch

--- Sandbox.java
+++ Sandbox.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 
 public class Sandbox {
-     public static void main(String[] args)throws ArithmeticException {
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Enter a number: ");
-          int firstNumber = scanner.nextInt();
-          System.out.println("Enter a second number: ");
-          int secondNumber = scanner.nextInt();
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter amount to withdraw: ");
+         int withdrawAmount = scanner.nextInt();
+         int accountBlanace = 100;
 
-          
-          try {
-               if (secondNumber == 0) {
-                    throw new ArithmeticException("You can not divide by zero!");
-               }
+         try {
+          if (withdrawAmount < 0) {
+               throw new IllegalArgumentException(
+                    "withdawal amount can not be negative!");
+          }
+          if (withdrawAmount > accountBlanace) {
+               throw new IllegalArgumentException(
+                    "Your withdraw amount is bigger than your account balance!");
+          }
+          accountBlanace = accountBlanace - withdrawAmount;
+          System.out.println(accountBlanace);
+          System.out.println("Your current balance is" + accountBlanace);
+         }
+         catch(IllegalArgumentException iae) {
+          System.out.println(iae.getMessage());
+         }
 
-               int finalResult = firstNumber/secondNumber;
-               System.out.println("The result of your computation is " + finalResult);
-          }
+         finally {
+          System.out.println("Transaction attempt finished!");
+          scanner.close();
+         }
 
-          catch (ArithmeticException ae) {
-                System.out.println(ae.getMessage());
-          }
-          finally {
-               System.out.println("Finished computing!");
-               scanner.close();
-          }
+
      }
 }
