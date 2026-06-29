354eff55c239fbc3d9b0647203107b66d9c12f5b
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-29 11:11:39 +0200

    Binary search

--- Sandbox.java
+++ Sandbox.java
@@ -1,26 +1,51 @@
 
 public class Sandbox {
      public static void main(String[] args) {
-          int [] array = {9, 1, 8, 2, 7, 3, 6, 5, 4};
 
-          insertionSort(array);
+          int [] ints = {1, 2, 4, 5, 7, 9, 11};
 
-          for (int i: array) {
-               System.out.println(i + " ");
-          }
+         System.out.println(binarySearch(ints, 9));
+
      }
 
-     private static void insertionSort(int[] array) {
-          for(int i = 1; i < array.length; i++) {
-               int temp = array[i];
-               int j = i-1;
+     private static int binarySearch(int[] numbers, int numberToFind) {
+          int low = 0;
+          int high = numbers.length - 1;
 
-               while (j >= 0 && array[j] > temp) {
-                    array[j+1] = array[j];
-                    j--;
+          while (low <= high) {
+               int middlePosition = (low + high) / 2;
+               int middleNumber = numbers[middlePosition];
+               if (numberToFind == middleNumber) {
+                    return middlePosition;
+               }
+               if (numberToFind < middleNumber) {
+                    high = middlePosition - 1;
+               } else {
+                    low = middlePosition + 1;
+               }
+          }
+          return -1;
+     }
+
+          
+     /*           int [] array = {3, 5, 2, 6, 7, 1};
+     
+               insertionSort(array);
+     
+               for (int i: array) {
+                    System.out.println(i);
+               }
+          }  
+          public static void insertionSort(int[] array) {
+               for (int i = 1; i < array.length; i++) {
+                    int temp = array[i];
+                    int j = i - 1;
+                    while (j >= 0 && array[j] > temp) {
+                         array[j+1] = array[j];
+                         j--;
                }
                array[j+1] = temp;
-          }
-     }
-        
+               } */
+
+
 }
