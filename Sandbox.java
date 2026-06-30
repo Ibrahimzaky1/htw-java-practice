a874a058e338863f19a185e0cbbe3ad8b58d69fc
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-06-30 08:04:28 +0200

    Calling arrays through methods

--- Sandbox.java
+++ Sandbox.java
@@ -2,80 +2,35 @@ import java.util.Arrays;
 
 public class Sandbox {
           public static void main(String[] args) {
-               int [] original = {1, 2, 3, 4, 5};
+               int [] array1 = initilize(5);
 
-               int [] biggerCopy = copy(original, 7);
-               int [] smallerCopy = copy(original, 3);
+               int [] array2 = initilize(9);
 
-               System.out.println("Bigger array copy: ");
-               for (int i : biggerCopy) {
-                    System.out.println(i + "");
-               }
+               System.out.println("Array 1: ");
+               printArray(array1);
 
-               System.out.println("Smaller array copy: ");
-               for (int i : smallerCopy) {
-                    System.out.println(i);
-               }
+               System.out.println("Array 2: ");
+               printArray(array2);
+
           }
 
-          public static int[] copy(int [] original, int newLength) {
-               int [] copy = new int[newLength];
 
-               if (original.length <= newLength) {
-                    for (int i = 0; i < original.length; i++) {
-                         copy[i] = original[i];
-                    }
+          public static int[] initilize (int length) {
+               int [] temp = new int[length];
+
+               for (int i = 0; i < temp.length; i++) {
+                    temp[i] = i;
                }
-               else {
-                    for (int i = 0; i < newLength; i++) {
-                         copy[i] = original[i];
-                    }
+               return temp;
+          }
+
+          public static void printArray(int [] array) {
+               for (int i : array) {
+                    System.out.print(i + " ");
                }
-               return copy;
+               System.out.println();
           }
 }
 
-     /*           int [] array = {3, 5, 2, 6, 7, 1};
-     
-               insertionSort(array);
-     
-               for (int i: array) {
-                    System.out.println(i);
-               }
-          }  
-          public static void insertionSort(int[] array) {
-               for (int i = 1; i < array.length; i++) {
-                    int temp = array[i];
-                    int j = i - 1;
-                    while (j >= 0 && array[j] > temp) {
-                         array[j+1] = array[j];
-                         j--;
-               }
-               array[j+1] = temp;
-               } */
 
-
-/*      private static int binarySearch(int[] numbers, int numberToFind) {
-     
-          int low = 0;
-          int high = numbers.length - 1;
-
-          while (low <= high) {
-               int middlePosition  = (low + high) / 2;
-               int middleNumber = numbers[middlePosition];
-
-               if (numberToFind == middleNumber) {
-                    return middlePosition;
-               }
-               if (numberToFind < middleNumber) {
-                    high = middlePosition - 1;
-               }
-               else {
-                    low = middlePosition + 1;
-               }
-          }
-          return -1;
-     } */
-
-      
 
