79c11c7d4b65c6d2f5bbce7799000b99e8229916
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-07-07 13:37:43 +0200

    New exercise

--- Sandbox.java
+++ Sandbox.java
@@ -3,49 +3,39 @@ import java.util.Scanner;
 
 public class Sandbox {
      public static void main(String[] args) {
-          int [] array = {10, 20, 30, 40, 50};
-          int [] array1 = null;
+          int [] array1 = {1,2, 3, 4, 5, 6, 7, 8, 9, 10};
+          int [] array2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
           try {
-               reversePair(array, 1);
-               printArray(array);
+               System.out.println("The length difference is: ");
+               System.out.println(laengenUnterschied(array1, array2));
           }
-          catch (NullPointerException e) {
-               System.out.println("Array can not be null!");
+          catch (ArithmeticException e) {
+          System.out.println("Length can not be negative!");
           }
-          catch (IndexOutOfBoundsException e) {
-               System.out.println("Invalid index");
-          }
-          try {
-               reversePair(array1, 1);
-               printArray(array1);
-          }
-          catch (NullPointerException e) {
-               System.out.println("Array can not be null!");
-          }
-          catch (IndexOutOfBoundsException e) {
-               System.out.println("Invalid index");
-          }
-          try {
-               reversePair(array, 4);
-               printArray(array);
-          }
-          catch(IndexOutOfBoundsException e) {
-               System.out.println();
-          }
-     }
-     public static void reversePair(int [] array, int index) {
-          if (array == null) {throw new NullPointerException();}
-          if (index < 0 || index > array.length - 1) {throw new IndexOutOfBoundsException();}
-          int temp = array[index];
-          array[index] = array[index + 1];
-          array[index + 1] = temp;
-     }
-
-     public static void printArray(int [] array) {
-          for (int i:array) {
-               System.out.print(i + " ");
-          }
+          System.out.println("Array 1 is: ");
+          unterZehnAusgaben(array1);
           System.out.println();
+          System.out.println("Array 2 is: ");
+          unterZehnAusgaben(array2);
+
+     }
+     public static int laengenUnterschied(int [] array1, int [] array2) {
+          
+          int lengthDifference = (array1.length - array2.length);
+          if (lengthDifference < 0) {
+               throw new ArithmeticException();
+          }
+          else {
+               return lengthDifference;
+          }
+     }
+     public static void unterZehnAusgaben(int [] array) {
+          for (int i = 0; i < array.length; i++) {
+               if (array[i] <= 10) {
+                    System.out.print(array[i]);
+               }
+               System.out.print(" ");
+          }
      }
 }
