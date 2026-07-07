66cdae63cd25c11e4e48863a18c61a43e4a22fb2
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-07-07 09:33:37 +0200

    Arrays and exception

--- Sandbox.java
+++ Sandbox.java
@@ -3,34 +3,56 @@ import java.util.Scanner;
 
 public class Sandbox {
      public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Enter first index");
-          int index1 = scanner.nextInt();
-          System.out.println("Enter second index: ");
-          int index2 = scanner.nextInt();
-
-          int [] array = {1, 2, 3, 4, 5, 6};
+          int [] array = {5, 10, 15, 20, 25};
+          int [] array2 = null;
 
           try {
-               swap(array, index1, index2);
+               swap(array, 1, 2);
                printArray(array);
           }
-          catch(IndexOutOfBoundsException e) {
+          catch (NullPointerException e) {
+               System.out.println("Array can not be null!");
+          }
+          catch (IndexOutOfBoundsException e) {
                System.out.println("Invalid index!");
           }
-          scanner.close();
+          try {
+               swap(array2, 1, 2);
+               printArray(array2);
+          }
+          catch (NullPointerException e) {
+               System.out.println("Array can not be null!");
+          } 
+          catch (IndexOutOfBoundsException e) {
+               System.out.println("Invalid index!");
+          }
+          try {
+               swap(array, 6, 7);
+               printArray(array);
+          }
+          
+          catch (NullPointerException e) {
+               System.out.println("Array can not be null!");
+          } 
+          catch (IndexOutOfBoundsException e) {
+               System.out.println("Invalid index!");
+          }
+          
+
      }
 
-     public static void swap(int [] array, int index1, int index2) {
-          if (index1 < 0 || index1 >= array.length || index2 < 0 || index2 >= array.length) {
-               throw new IndexOutOfBoundsException();
-          }
-          int temp = array[index1];
-          array[index1] = array[index2];
-          array[index2] = temp;
+     public static void swap(int [] array, int index1, int index2) throws NullPointerException {
+          if (array == null) {throw new NullPointerException();};
+          if (index1 < 0 || index1 >= array.length 
+               || index2 < 0 || index2 >= array.length) {
+                    throw new IndexOutOfBoundsException();
+               }
+               int temp = array[index1];
+               array[index1] = array[index2];
+               array[index2] = temp;
      }
      public static void printArray(int [] array) {
-          for (int i : array) {
+          for (int i:array) {
                System.out.print(i + " ");
           }
           System.out.println();
