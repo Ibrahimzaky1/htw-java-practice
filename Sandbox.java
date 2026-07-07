bc5dd37b5b3da2fe5c1082b34db643e9a76f7f09
Ibrahim Zaky <Ibrahimzaky2000@gmail.com>
2026-07-07 07:30:29 +0200

    Swapping elements in array

--- Sandbox.java
+++ Sandbox.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class Sandbox {
      public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
-          System.out.println("Give an index");
-          int userInput = scanner.nextInt();
+          System.out.println("Enter first index");
+          int index1 = scanner.nextInt();
+          System.out.println("Enter second index: ");
+          int index2 = scanner.nextInt();
 
-          int [] array = {1, 2, 3, 4, 5};
+          int [] array = {1, 2, 3, 4, 5, 6};
 
           try {
-               int number = getElement(array, userInput);
-               System.err.println("index is: " + number);
+               swap(array, index1, index2);
+               printArray(array);
           }
           catch(IndexOutOfBoundsException e) {
                System.out.println("Invalid index!");
@@ -19,11 +21,19 @@ public class Sandbox {
           scanner.close();
      }
 
-     public static int getElement(int [] array, int index) {
-          if (index < 0 || index >= array.length) {
+     public static void swap(int [] array, int index1, int index2) {
+          if (index1 < 0 || index1 >= array.length || index2 < 0 || index2 >= array.length) {
                throw new IndexOutOfBoundsException();
           }
-          return array[index];
+          int temp = array[index1];
+          array[index1] = array[index2];
+          array[index2] = temp;
+     }
+     public static void printArray(int [] array) {
+          for (int i : array) {
+               System.out.print(i + " ");
+          }
+          System.out.println();
      }
 }
-             
+ 
